55cf3d2fd8abf3029634f297ec10bcfe11fb0b53
nikitasol <nikita@handrich.es>
2021-11-19 18:33:43 +0100

    Update google-analytics.php

--- resources/views/google-analytics.php
+++ resources/views/google-analytics.php
@@ -1,16 +1,13 @@
 <?php // phpcs:disable ?>
-<script>
-  <?php if ($should_load) : ?>
-    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-  <?php else : ?>
-    (function(s,o,i,l){s.ga=function(){s.ga.q.push(arguments);if(o['log'])o.log(i+l.call(arguments))}
-    s.ga.q=[];s.ga.l=+new Date;}(window,console,'Google Analytics: ',[].slice))
-  <?php endif; ?>
-  ga('create','<?= $google_analytics_id; ?>','auto');
-  <?php if ($optimize_id) : ?>ga('require','<?= $optimize_id; ?>');<?php endif; ?>
-  <?php if ($anonymize_ip) : ?>ga('set','anonymizeIp',true);<?php endif; ?>
-  ga('set','transport','beacon');ga('send','pageview');
-</script>
 <?php if ($should_load) : ?>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?= $ga4_id ?>">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?= $ga4_id ?>');
+  gtag('config', '<?= $google_analytics_id ?>'{<?php if ($anonymize_ip) : ?>, 'anonymize_ip': true<?php endif; ?><?php if ($optimize_id) : ?>, 'optimize_id': '<?= $optimize_id; ?>'});<?php endif; ?>
+</script>
 <?php endif; ?>
